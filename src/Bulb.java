f176436a49a884fb265e1d59adc5dc9dbdcbc834
Vignal-Fanny <fanny.vignal@iut-valence.fr>
2014-02-10 11:31:44 +0100

    Saving after class

--- src/Bulb.java
+++ src/Bulb.java
@@ -1,17 +1,35 @@
 
 public class Bulb
 {
+	/**
+	 * The default power of the bulbs.
+	 */
 	private static final int DEFAULT_POWER = 100;
+	/**
+	 * The chosen power of the bulb.
+	 */
 	private final int power;
+	/**
+	 * the state of the bulb (lit/not-lit).
+	 */
 	private boolean islit;
+	/**
+	 * The state of the bulb (burnt/not-burnt).
+	 */
 	private boolean isburnt;
 	
+	/**
+	 * Creates a not-lit and not-burnt bulb with default power.
+	 */
 	public Bulb()
 	{
 		this.power = Bulb.DEFAULT_POWER;
 		this.islit = false;
 		this.isburnt = false;
 	}
+	/** Creates a not-lit and not-burnt bulb with a chosen power.
+	 * @param powerValue The power of the bulb.
+	 */
 	public Bulb(int powerValue)
 	{
 		this.power = powerValue;
@@ -19,12 +37,19 @@ public class Bulb
 		this.isburnt = false;
 	}
 	
+	/** Creates a not-lit bulb with default power and with chosen state (burnt/not-burnt).
+	 * @param burntValue The state of the bulb (burnt/not-burnt).
+	 */
 	public Bulb(boolean burntValue)
 	{
 		this.power = Bulb.DEFAULT_POWER;
 		this.islit = false;
 		this.isburnt = burntValue;
 	}
+	/** Creates a not-lit bulb with chosen power and chosen state (burnt/not-burnt).
+	 * @param powerValue 
+	 * @param burntValue
+	 */
 	public Bulb(int powerValue, boolean burntValue)
 	{
 		this.power = powerValue;
